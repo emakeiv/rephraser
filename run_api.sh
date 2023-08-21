134583875f5d3db3c1ed1fc79736ad337dc4f041
Eivanas Maksvytis <eivanas.m@gmail.com>
2023-08-21 12:36:06 +0000

    __23_08_21__fill_gitignore_

--- run_api.sh
+++ run_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source .venv/bin/activate
+pipenv shell
 
 # Start the FastAPI application using Uvicorn
 uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
